4654f25e6a5f7a2e9fc1db0e4af73859fad99551
SwastikChamp2 <130574319+SwastikChamp2@users.noreply.github.com>
2023-05-08 08:22:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedsignuppagewidget/GeneratedSignupPageWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedloginpagewidget/GeneratedLoginPageWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedchatpagewidget/GeneratedChatPageWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedchatpagewidget1/GeneratedChatPageWidget1.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedchatpagewidget2/GeneratedChatPageWidget2.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedchatpagewidget3/GeneratedChatPageWidget3.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedchatpagewidget4/GeneratedChatPageWidget4.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedtypebadgeswidget5/GeneratedTypeBadgesWidget5.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedcolourpalletewidget/GeneratedColourpalleteWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generated_circlebuttonswidget9/Generated_CircleButtonsWidget9.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedredbookmarkwidget5/GeneratedRedbookmarkWidget5.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedbottombuttonwidget36/GeneratedBottomButtonWidget36.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedbottombuttonsandrectanglewidget9/GeneratedBottombuttonsandrectangleWidget9.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatediconssearch_24pxwidget2/GeneratedIconssearch_24pxWidget2.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedselecteventtypewidget2/GeneratedSelectEventTypeWidget2.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventiconswidget48/GeneratedEventIconsWidget48.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedcheckboxeswidget/GeneratedCheckboxesWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedindividualinterestswidget/GeneratedIndividualInterestsWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedselecteventtypewidget3/GeneratedSelectEventTypeWidget3.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedlistwidget22/GeneratedListWidget22.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generateduserprofilepicwidget11/GeneratedUserprofilepicWidget11.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedinputchipwidget9/GeneratedInputchipWidget9.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedmembersdropdownwidget6/GeneratedMembersdropdownWidget6.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatededitwidget/GeneratedEditWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedediticonwidget2/GeneratedEditIconWidget2.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedediticongreywidget10/GeneratedEditIconGreyWidget10.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatededitlocationwidget/GeneratedEditlocationWidget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedmessageiconwidget5/GeneratedMessageiconWidget5.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard1widget2/GeneratedEventCard1Widget2.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard2widget/GeneratedEventCard2Widget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard3widget1/GeneratedEventCard3Widget1.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard4widget/GeneratedEventCard4Widget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard5widget/GeneratedEventCard5Widget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard6widget3/GeneratedEventCard6Widget3.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard7widget3/GeneratedEventCard7Widget3.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard8widget/GeneratedEventCard8Widget.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedeventcard9widget1/GeneratedEventCard9Widget1.dart';
+import 'package:flutterapp/undigitally_20frontendapp/generatedpeoplesuggestionwidget/GeneratedPeopleSuggestionWidget.dart';
+
+void main() {
+  runApp(Undigitally_20FrontendApp());
+}
+
+class Undigitally_20FrontendApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedSignupPageWidget',
+      routes: {
+        '/GeneratedSignupPageWidget': (context) => GeneratedSignupPageWidget(),
+        '/GeneratedLoginPageWidget': (context) => GeneratedLoginPageWidget(),
+        '/GeneratedChatPageWidget': (context) => GeneratedChatPageWidget(),
+        '/GeneratedChatPageWidget1': (context) => GeneratedChatPageWidget1(),
+        '/GeneratedChatPageWidget2': (context) => GeneratedChatPageWidget2(),
+        '/GeneratedChatPageWidget3': (context) => GeneratedChatPageWidget3(),
+        '/GeneratedChatPageWidget4': (context) => GeneratedChatPageWidget4(),
+        '/GeneratedTypeBadgesWidget5': (context) =>
+            GeneratedTypeBadgesWidget5(),
+        '/GeneratedColourpalleteWidget': (context) =>
+            GeneratedColourpalleteWidget(),
+        '/Generated_CircleButtonsWidget9': (context) =>
+            Generated_CircleButtonsWidget9(),
+        '/GeneratedRedbookmarkWidget5': (context) =>
+            GeneratedRedbookmarkWidget5(),
+        '/GeneratedBottomButtonWidget36': (context) =>
+            GeneratedBottomButtonWidget36(),
+        '/GeneratedBottombuttonsandrectangleWidget9': (context) =>
+            GeneratedBottombuttonsandrectangleWidget9(),
+        '/GeneratedIconssearch_24pxWidget2': (context) =>
+            GeneratedIconssearch_24pxWidget2(),
+        '/GeneratedSelectEventTypeWidget2': (context) =>
+            GeneratedSelectEventTypeWidget2(),
+        '/GeneratedEventIconsWidget48': (context) =>
+            GeneratedEventIconsWidget48(),
+        '/GeneratedCheckboxesWidget': (context) => GeneratedCheckboxesWidget(),
+        '/GeneratedIndividualInterestsWidget': (context) =>
+            GeneratedIndividualInterestsWidget(),
+        '/GeneratedSelectEventTypeWidget3': (context) =>
+            GeneratedSelectEventTypeWidget3(),
+        '/GeneratedListWidget22': (context) => GeneratedListWidget22(),
+        '/GeneratedUserprofilepicWidget11': (context) =>
+            GeneratedUserprofilepicWidget11(),
+        '/GeneratedInputchipWidget9': (context) => GeneratedInputchipWidget9(),
+        '/GeneratedMembersdropdownWidget6': (context) =>
+            GeneratedMembersdropdownWidget6(),
+        '/GeneratedEditWidget': (context) => GeneratedEditWidget(),
+        '/GeneratedEditIconWidget2': (context) => GeneratedEditIconWidget2(),
+        '/GeneratedEditIconGreyWidget10': (context) =>
+            GeneratedEditIconGreyWidget10(),
+        '/GeneratedEditlocationWidget': (context) =>
+            GeneratedEditlocationWidget(),
+        '/GeneratedMessageiconWidget5': (context) =>
+            GeneratedMessageiconWidget5(),
+        '/GeneratedEventCard1Widget2': (context) =>
+            GeneratedEventCard1Widget2(),
+        '/GeneratedEventCard2Widget': (context) => GeneratedEventCard2Widget(),
+        '/GeneratedEventCard3Widget1': (context) =>
+            GeneratedEventCard3Widget1(),
+        '/GeneratedEventCard4Widget': (context) => GeneratedEventCard4Widget(),
+        '/GeneratedEventCard5Widget': (context) => GeneratedEventCard5Widget(),
+        '/GeneratedEventCard6Widget3': (context) =>
+            GeneratedEventCard6Widget3(),
+        '/GeneratedEventCard7Widget3': (context) =>
+            GeneratedEventCard7Widget3(),
+        '/GeneratedEventCard8Widget': (context) => GeneratedEventCard8Widget(),
+        '/GeneratedEventCard9Widget1': (context) =>
+            GeneratedEventCard9Widget1(),
+        '/GeneratedPeopleSuggestionWidget': (context) =>
+            GeneratedPeopleSuggestionWidget(),
+      },
+    );
+  }
+}
